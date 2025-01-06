2857f3a9a4314ffba29e421ffded599675c53af9
Stunnerer <semgoll824@gmail.com>
2025-01-06 12:51:10 +0300

    Fix ImageFirework
    - fixed constructor as im stupid
    - fixed field name as it differs from config
    - added toString for logging purposes

--- src/main/kotlin/com/joutak/jtny/dto/ImageFirework.kt
+++ src/main/kotlin/com/joutak/jtny/dto/ImageFirework.kt
@@ -1,14 +1,12 @@
 package com.joutak.jtny.dto
 
+import com.joutak.jtny.JouTakNewYear
 import org.bukkit.configuration.serialization.ConfigurationSerializable
 
 
-class ImageFirework(name: String, imageName: String, modelData: Int, displayTime: Int, flightDuration: Int) : ConfigurationSerializable {
-    val name = ""
-    val imageName = ""
-    val modelData = 0
-    val displayTime = 1
-    val flightDuration = 3
+class ImageFirework(val name: String, val imageName: String, val modelData: Int, val displayTime: Int,
+                    val flightDuration: Int
+) : ConfigurationSerializable {
 
     override fun serialize(): Map<String, Any> {
         val data: MutableMap<String, Any> = HashMap()
@@ -23,14 +21,18 @@ class ImageFirework(name: String, imageName: String, modelData: Int, displayTime
     }
 
     companion object {
-        fun deserialize(args: Map<String?, Any?>): ImageFirework {
+        fun deserialize(args: Map<String, Any>): ImageFirework {
             return ImageFirework(
                 args["name"] as String,
                 args["imageName"] as String,
-                args["customModelData"] as Int,
+                args["modelData"] as Int,
                 args["displayTime"] as Int,
                 args["flightDuration"] as Int,
             )
         }
+    }
+
+    override fun toString(): String {
+        return "ImageFirework(name=$name, imageName=$imageName, modelData=$modelData, displayTime=$displayTime, flightDuration=$flightDuration)"
     }
 }
